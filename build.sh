f20bdac2abe81125df8e0ef62e89457da393e87c
vishnu-k-s <58019613+vishnu-k-s@users.noreply.github.com>
2023-11-23 23:34:23 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -o errexit
 
 pip install -r requirements.txt
 pip install gunicorn
-#python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 python manage.py migrate
